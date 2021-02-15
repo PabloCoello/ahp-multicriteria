d047127e629c3aaacfa443bd94697c5773b28761
Pablo Coello <pablo.coellopulido@outlook.es>
2021-02-15 13:05:33 +0100

    Update

--- prueba.py
+++ prueba.py
@@ -3,52 +3,40 @@ import numpy as np
 from saaty import ahp
 
 
-def get_attribute_submatrix(attribute, pow_value):
-    toret = pd.DataFrame(columns=x.index, index=x.index)
-    for index, j in x.iteritems():
-        col = [i/j for i in x]
-        toret[index] = col
-    toret = get_priorities(toret, pow_value)
-    return toret
-
-def get_priorities(matrix, pow_value):
-    '''
-    Normalise matrix given by get_matrix()
-    '''
-    subm = matrix.pow(pow_value)
-    sum_array = [sum(row[1]) for row in subm.iterrows()]
-    priorities = [elem/np.sum(sum_array) for elem in sum_array]
-    matrix['priorities'] = priorities
-    return matrix
-
-df = pd.read_excel('./data/dummy_strat.xlsx')
-df.set_index('Alternatives', inplace=True)
-df = get_attribute_submatrix(df.econ)
-prio = get_priorities(df, 10)
-
 
 data = pd.read_excel("data/dummy_survey_data.xlsx")
-dummy = pd.read_excel("data/dummy_strat.xlsx")
+dummy = pd.read_excel("data/dummy_asneves.xlsx")
 squema = {
         "main":{
             "range":[1,13],
             "attributes":["econ","social","env","coste"]
-        },
-        "env":{
-            "range":[13,19],
-            "attributes":["nocons", "zepaozec", "zepayzec"]
         }
     }
-
-    dummy
 
 res = ahp(
   train=True,
   data=data,
   pow_value=10,
   confidence=0.95,
-  cratio_threshold=0.1,
+  cratio_threshold=1,
   squema=squema
 )
+res.squema
+res.weights
+res.cr
+res.weights['main'].cr_ratio.hist()
+df = res.set_ahp_weights(dummy, inverse = ['coste'])
 
-res.set_ahp_weights(dummy)
+serie = df['coste'].sort_values(ascending=True)
+val = serie.sort_values(ascending=False).values
+for row, index in zip(serie.iteritems(),range(len(serie))):
+  serie.at[row[0]] = val[index]
+df['coste'] = serie
+
+1-df['coste']
+res.priorities_matrix
+pairwise = res.attribute_pairwise_matrix
+pairwise['econ']
+pairwise['env']
+pairwise['social']
+pairwise['econ']
